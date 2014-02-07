c8df98f463a2e1d3f5dd08221afa9cb3db5fbc75
Kevin Ali <kevin.adil.ali@gmail.com>
2014-02-07 16:26:41 -0500

    Initial commit

---
+++ db/migrate/20140207204613_create_restaurants.rb
@@ -0,0 +1,14 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :neighbourhood
+      t.string :price
+      t.text :summary
+      t.string :cuisine
+
+      t.timestamps
+    end
+  end
+end
